15131eaace9409fd1f25ae181c88a51156f9d719
Eden Reich <eden.reich@gmail.com>
2019-09-15 18:37:10 +0200

    add forward declartion

--- src/http/interfaces/client_interface.h
+++ src/http/interfaces/client_interface.h
@@ -1,9 +1,15 @@
 #ifndef CLIENT_INTERFACE_H
 #define CLIENT_INTERFACE_H
 
+#include "response_interface.h"
+#include "url_interface.h"
+
 
 namespace Http {
     namespace Interfaces {
+
+        class UrlInterface;
+        class ResponseInterface;
 
         /**
          * The Client Interface
@@ -11,7 +17,14 @@ namespace Http {
         class ClientInterface {
 
         public:
-            //
+            
+            /**
+             * Get the response by given url.
+             * 
+             * @param UrlInterface url
+             * @return ResponseInterface
+             */
+            virtual ResponseInterface get(UrlInterface & url) const = 0;
 
         };
     }
