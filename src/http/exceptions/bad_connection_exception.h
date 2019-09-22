d1489ab2faf6d2d9a0fe493d8afc5560610d63c1
Eden Reich <eden.reich@gmail.com>
2019-09-22 23:37:23 +0200

    add custom messages

--- src/http/exceptions/bad_connection_exception.h
+++ src/http/exceptions/bad_connection_exception.h
@@ -10,11 +10,32 @@ class BadConnectionException : public std::exception
 public:
 
     /**
+     * Default constructor.
+     */
+    BadConnectionException() : m_message("Could not connect to server.") {}
+
+    /**
+     * Initialize a custom message.
+     * 
+     * @param char const * message
+     */
+    BadConnectionException(char const * message) noexcept : m_message(message) {}
+
+    /**
      * Retrieve the reason.
      * 
      * @return char const *
      */
-    char const * what() const throw() override { return "Could not connect to server."; }
+    char const * what() const throw() override { return m_message; }
+
+private:
+
+    /**
+     * Store the custom message.
+     * 
+     * @var char const *
+     */
+    char const * m_message;
 
 };
 
