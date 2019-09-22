f42ec5a372f25f32e28b1e06556c19e5c31cf08f
Eden Reich <eden.reich@gmail.com>
2019-09-23 01:00:33 +0200

    check if we are on windows

--- src/server.cpp
+++ src/server.cpp
@@ -27,28 +27,32 @@ Server::Server() : m_server_fd(::socket(AF_INET, SOCK_STREAM, 0)), m_address(std
  */
 void Server::bind(const std::string & address)
 {
-    int opt = 1;
-    m_address = address;
-    m_localaddr.sin_family = AF_INET;
-    m_localaddr.sin_addr.s_addr = inet_addr(m_address.c_str());
-    m_localaddr.sin_port = htons(8080); 
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+        // @todo write windows specific implemention
+    #else
+        int opt = 1;
+        m_address = address;
+        m_localaddr.sin_family = AF_INET;
+        m_localaddr.sin_addr.s_addr = inet_addr(m_address.c_str());
+        m_localaddr.sin_port = htons(8080); 
 
-    if (::setsockopt(m_server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
-    {
-        throw BadConnectionException("Setsockopt failed");
-    } 
+        if (::setsockopt(m_server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
+        {
+            throw BadConnectionException("Setsockopt failed");
+        } 
 
-    int failed = ::bind(m_server_fd, (struct sockaddr *)&m_localaddr, sizeof(m_localaddr));
+        int failed = ::bind(m_server_fd, (struct sockaddr *)&m_localaddr, sizeof(m_localaddr));
 
-    if (m_server_fd == 0) 
-    { 
-        throw BadConnectionException("Could not bind the server to address!");
-    }
+        if (m_server_fd == 0) 
+        { 
+            throw BadConnectionException("Could not bind the server to address!");
+        }
 
-    if (failed < 0) 
-    { 
-        throw BadConnectionException("Could not bind to the given port");
-    } 
+        if (failed < 0) 
+        { 
+            throw BadConnectionException("Could not bind to the given port");
+        }
+    #endif
 }
 
 /**
@@ -59,10 +63,14 @@ void Server::bind(const std::string & address)
  */
 void Server::listen(const unsigned int & port)
 {
-    if (::listen(m_server_fd, 3) < 0) 
-    { 
-        throw BadConnectionException("Could not listen on the given port");
-    }
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+        // @todo write windows specific implemention
+    #else
+        if (::listen(m_server_fd, 3) < 0) 
+        { 
+            throw BadConnectionException("Could not listen on the given port");
+        }
+    #endif
 
     m_running = true;
 }
@@ -75,22 +83,26 @@ void Server::listen(const unsigned int & port)
  */
 void Server::onMessage(Events::MessageRecievedHandler callback) const
 {
-    int clientConnection;
-    char buffer[1024] = {0};
-    int addresslen = sizeof(m_localaddr);
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+        // @todo write windows specific implemention
+    #else
+        int clientConnection;
+        char buffer[1024] = {0};
+        int addresslen = sizeof(m_localaddr);
 
-    while (m_running) {
-        if ((clientConnection = ::accept(m_server_fd, (struct sockaddr *)&m_localaddr, (socklen_t*)&addresslen)) < 0)
-        {
-            throw BadConnectionException("Could not accept the connection");
+        while (m_running) {
+            if ((clientConnection = ::accept(m_server_fd, (struct sockaddr *)&m_localaddr, (socklen_t*)&addresslen)) < 0)
+            {
+                throw BadConnectionException("Could not accept the connection");
+            }
+
+            int dataLength = ::read(clientConnection, buffer, 1024);
+
+            callback(this, buffer);
+
+            ::close(clientConnection);
         }
-
-        int dataLength = ::read(clientConnection, buffer, 1024);
-
-        callback(this, buffer);
-
-        ::close(clientConnection);
-    }
+    #endif
 }
 
 /**
